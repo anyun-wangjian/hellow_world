21e85a699ca8516afab3f17d3616daad132d7c58
wangjian <wangjian@jsanyun.com>
2018-04-11 16:03:12 +0800

    修改错误之处

--- src/com/hit/demo/HelloWorld.java
+++ src/com/hit/demo/HelloWorld.java
@@ -14,7 +14,7 @@ package com.hit.demo;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("args = [" + wangjian + "]");
+        System.out.println("wangjian");
     }
 
 }
