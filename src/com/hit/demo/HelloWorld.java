36bab85f85efbea1eeaef653b5b255f709f8fc10
wangjian <wangjian@jsanyun.com>
2018-04-11 15:54:27 +0800

    Initial commit

---
+++ src/com/hit/demo/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * ==============================================================
+ * Copyright (c) 2012 ~ 2018. anyuncloud.com All rights reserved.
+ *                                           Created by wangjian
+ * ==============================================================
+ */
+
+package com.hit.demo;
+
+/**
+ * Created by wangjian
+ * Date: 18-4-11
+ * Time: 下午3:16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args.length + "]");
+    }
+
+}
